064c0409e91d27d66bb6f82b259a9775a3414d63
kodaishiotsuki <0711dr5@gmail.com>
2022-07-14 17:38:50 +0900

    複数のreducerの組み合わせ

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import allReducers from './reducers';
 
+//store
+const store = createStore(allReducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
