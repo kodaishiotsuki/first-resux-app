72f6f94ecb86006bd542099de44322aaed12b3c4
kodaishiotsuki <0711dr5@gmail.com>
2022-07-14 21:26:04 +0900

    状態更新のためのUI作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   //counterにアクセス
   const counter = useSelector((state) => state.counter);
   const isLogin = useSelector((state) => state.isLogin);
+  //dispatch
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント:{counter}</h3>
+      <button onClick={() => dispatch()}> + </button>
       {isLogin ? <h3>ログイン成功</h3> : <h3>ログインしてください</h3>}
     </div>
   );
