66dc1ec168582c9127b23d19a069824777f5d80b
kodaishiotsuki <0711dr5@gmail.com>
2022-07-14 21:21:27 +0900

    useSelectorのHooksを使って状態にアクセス

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  //counterにアクセス
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+
   return (
     <div className="App">
       <h1>Hello Redux</h1>
+      <h3>カウント:{counter}</h3>
+      {isLogin ? <h3>ログイン成功</h3> : <h3>ログインしてください</h3>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* Providerを使うことでどのコンポーネントにもアクセスできる */}
-    <Provider store={store}> 
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
