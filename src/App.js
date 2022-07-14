c8f7747829c10bc07f618df367e84d9a834ee429
kodaishiotsuki <0711dr5@gmail.com>
2022-07-14 21:43:46 +0900

    Actionにpayload追加

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント:{counter}</h3>
-      <button onClick={() => dispatch(increment())}> + </button>
+      <button onClick={() => dispatch(increment(10))}> + </button>
       <button onClick={() => dispatch(decrement())}> - </button>
       {isLogin ? <h3>ログイン成功</h3> : <h3>ログインしてください</h3>}
       <button onClick={() => dispatch(login())}>ログイン or ログアウト</button>
