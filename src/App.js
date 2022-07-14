1c85d1acb9dd9a00606a5b6f5096105f1ecaee8e
kodaishiotsuki <0711dr5@gmail.com>
2022-07-14 21:38:50 +0900

    Actionを作成してuseDispatchでStoreへ通知を出力

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, login } from "./actions";
 
 function App() {
   //counterにアクセス
@@ -12,8 +13,10 @@ function App() {
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント:{counter}</h3>
-      <button onClick={() => dispatch()}> + </button>
+      <button onClick={() => dispatch(increment())}> + </button>
+      <button onClick={() => dispatch(decrement())}> - </button>
       {isLogin ? <h3>ログイン成功</h3> : <h3>ログインしてください</h3>}
+      <button onClick={() => dispatch(login())}>ログイン or ログアウト</button>
     </div>
   );
 }
